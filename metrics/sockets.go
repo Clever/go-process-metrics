6845cfa269316a6ccb1692b1dc451a754a09ec07
Christopher Bradhaw <christopher.bradshaw@clever.com>
2016-09-30 23:13:10 +0000

    add comment

--- metrics/sockets.go
+++ metrics/sockets.go
@@ -25,6 +25,8 @@ func getSocketCount() uint64 {
 	hasLogged := false
 	for _, fd := range fds {
 		sl, err := os.Readlink(fmt.Sprintf("%s/%s", base, fd.Name()))
+		// The fd opened by ReadDir is often closed at this point.
+		// In general, fds may close between the check and readlink and we should ignore those
 		if err != nil && !strings.HasSuffix(err.Error(), "no such file or directory") {
 			if !hasLogged {
 				lg.ErrorD("failed-readlink", logger.M{"err": err.Error()})
