d8f2eac5a037f27995c56753845d440e9ceb034f
Christopher Bradhaw <christopher.bradshaw@clever.com>
2016-09-30 21:22:34 +0000

    add FD metric and logging

--- metrics/sockets.go
+++ metrics/sockets.go
@@ -5,7 +5,11 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"gopkg.in/Clever/kayvee-go.v3/logger"
 )
+
+var lg = logger.New("go-process-metrics")
 
 // getSocketCount returns the number of open sockets
 func getSocketCount() uint64 {
@@ -14,14 +18,18 @@ func getSocketCount() uint64 {
 	base := fmt.Sprintf("/proc/%d/fd", pid)
 	fds, err := ioutil.ReadDir(base)
 	if err != nil {
-		// Should we log something here?
+		lg.ErrorD("failed-get-fds", logger.M{"err": err.Error()})
 		return 0
 	}
 
+	hasLogged := false
 	for _, fd := range fds {
 		sl, err := os.Readlink(fmt.Sprintf("%s/%s", base, fd.Name()))
 		if err != nil {
-			// Should we log something here?
+			if !hasLogged {
+				lg.ErrorD("failed-readlink", logger.M{"err": err.Error()})
+				hasLogged = true
+			}
 			continue
 		}
 
@@ -30,4 +38,15 @@ func getSocketCount() uint64 {
 		}
 	}
 	return socketCount
+}
+
+func getFDCount() uint64 {
+	pid := os.Getpid()
+	base := fmt.Sprintf("/proc/%d/fd", pid)
+	fds, err := ioutil.ReadDir(base)
+	if err != nil {
+		lg.ErrorD("failed-get-fds", logger.M{"err": err.Error()})
+		return 0
+	}
+	return uint64(len(fds))
 }
